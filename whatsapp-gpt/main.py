228f127a5b45c7e7c521fb92afa04d7876b43476
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-04-07 23:49:36 -0500

    Integrate chatGPT

--- whatsapp-gpt/main.py
+++ whatsapp-gpt/main.py
@@ -1,22 +1,38 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-import json
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_KEY')
  
 app = Flask(__name__)
  
- 
+def forward_to_chatgpt(prompt: str) -> str:
+    model_engine = "gpt-3.5-turbo"
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        temperature=0.6,
+    )
+    response = completion.choices[0].text
+    return response
+
 @app.route("/message", methods=['POST'])
 def wa_sms_reply():
         
     msg = request.form.get('Body').lower()
  
-    print("msg-->",msg)
+    print("msg-->", msg)
     resp = MessagingResponse()
     reply=resp.message()
     
-    if msg == "hi":
-       reply.body("hello!")
- 
+    gpt_resp = forward_to_chatgpt(msg)
+    reply.body(gpt_resp) 
  
     return str(resp)
  
