77606f90441211c9d8918380d87c81b9ff3fe70c
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-04-11 18:41:53 -0500

    Use ChatCompletion class

--- whatsapp-gpt/main.py
+++ whatsapp-gpt/main.py
@@ -7,34 +7,40 @@ import openai
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_KEY')
- 
+
 app = Flask(__name__)
- 
-def forward_to_chatgpt(prompt: str) -> str:
-    model_engine = "gpt-3.5-turbo"
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
+
+
+def forward_to_chatgpt(message: str) -> str:
+    model = "gpt-3.5-turbo"
+    prompt = [{"role": "user", "content": message}]
+    
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=prompt,
         max_tokens=1024,
         n=1,
         temperature=0.6,
     )
-    response = completion.choices[0].text
+    response = completion.choices[0].message.content
+    print(response)
     return response
+
 
 @app.route("/message", methods=['POST'])
 def whatsapp_reply():
-        
+
     msg = request.form.get('Body').lower()
- 
+
     print("msg-->", msg)
     resp = MessagingResponse()
-    reply=resp.message()
-    
+    reply = resp.message()
+
     gpt_resp = forward_to_chatgpt(msg)
-    reply.body(gpt_resp) 
- 
+    reply.body(gpt_resp)
+
     return str(resp)
- 
-if __name__ == "__main__":	
+
+
+if __name__ == "__main__":
     app.run(debug=True)
